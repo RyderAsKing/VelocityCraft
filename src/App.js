7aeef0b28f7ea2ed1c9009eae06a9e7589484e05
RyderAsking <clasherlodt@gmail.com>
2021-08-11 14:57:35 +0530

    Basic route setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Switch from "./components/CustomSwitch";
+import Home from "./pages/Home";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home}></Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
