7fa709be94b994dc3ac001bae704b74f608dba0b
RyderAsking <clasherlodt@gmail.com>
2021-08-11 15:17:36 +0530

    Basic Website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Switch from "./components/CustomSwitch";
 import Home from "./pages/Home";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,60 @@
 import React from "react";
-
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="logo" style={{ textAlign: "center" }}>
+        <img src="img/image.png" alt="MyServer logo" />
+      </div>
+      <div className="items">
+        <a href="#" className="item forums">
+          <div>
+            <img
+              src="img/forums.png"
+              alt="Minecraft forums icon"
+              className="img"
+            />
+            <p className="subtitle">chat on our</p>
+            <p className="title">Forums</p>
+          </div>
+        </a>
+        <a href="#" className="item store">
+          <div>
+            <img
+              src="img/store.png"
+              alt="Minecraft store icon"
+              className="img"
+            />
+            <p className="subtitle">donate on our</p>
+            <p className="title">Store</p>
+          </div>
+        </a>
+        <a href="#" className="item vote">
+          <div>
+            <img
+              src="img/vote.png"
+              alt="Minecraft voting icon"
+              className="img"
+            />
+            <p className="subtitle">support us by</p>
+            <p className="title">Voting</p>
+          </div>
+        </a>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <div className="playercount">
+          <p>
+            Join{" "}
+            <span
+              className="sip"
+              data-ip="play.velocitycraft.net"
+              data-port={25565}
+            ></span>{" "}
+            other players on <span className="ip">play.velocitycraft.net</span>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
