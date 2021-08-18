554280e5b6927d45bb48f6b74ecc9acbf32007d2
RyderAsking <clasherlodt@gmail.com>
2021-08-18 14:46:19 +0530

    Update

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+let t;
+$(document).ready(() => {
+  t = $(".ip").html();
+});
+
+$(document).on("click", ".ip", () => {
+  let copy = document.createElement("textarea");
+  copy.style.position = "absolute";
+  copy.style.left = "-99999px";
+  copy.style.top = "0";
+  copy.setAttribute("id", "ta");
+  document.body.appendChild(copy);
+  copy.textContent = t;
+  copy.select();
+  document.execCommand("copy");
+  $(".ip").html("<span class='extrapad'>IP copied!</span>");
+  setTimeout(() => {
+    $(".ip").html(t);
+    var copy = document.getElementById("ta");
+    copy.parentNode.removeChild(copy);
+  }, 800);
+});
--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Switch from "./components/CustomSwitch";
 import Home from "./pages/Home";
 import Vote from "./pages/Vote";
-import Enchants from "./pages/Enchants";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Switch>
           <Route path="/" component={Home} exact></Route>
           <Route path="/vote" component={Vote} exact></Route>
-          <Route path="/enchant" component={Enchants} exact></Route>
         </Switch>
       </Router>
     </>
